73e020d5b5d7cf5ba830eeb62e9eb7a501631a38
SteveF <stvreeno_3@yahoo.com>
2021-11-30 14:01:18 -0500

    section10_JS

---
+++ section11_JS/app.js
@@ -0,0 +1,7 @@
+let courseName = '100 Days of Code';
+let coursePrice = 9.99;
+let courseGoals = ['Finish under 100 days\n ','Learn JS basics\n ','Get paid as developer'];
+let courseObj = { name: '100 Days of Code', price: 9.99, goals: courseGoals };
+
+// alert(courseName + '\n$ ' + coursePrice + '\n' + courseGoals[0]  + '\n' + courseGoals[1] + '\n' + courseGoals[2]);
+alert(courseObj.goals);
